81d7154f2be24bb0355628badd9047f94c2d1888
Rachit <rachitmathur8@gmail.com>
2020-07-21 21:47:50 +0530

    add https

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 var app = require("express")();
 var cors = require("cors");
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // var http = require("http").createServer(app);
 var server = app.listen(3000, () => {
   console.log("Server is listening on port: 3000");
