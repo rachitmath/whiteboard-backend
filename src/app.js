1086570a07d5900c3fa5e439c72f49e9765a41f4
Rachit <rachitmathur8@gmail.com>
2020-07-21 22:15:28 +0530

    fix minor issues

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 var app = require("express")();
 var cors = require("cors");
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(cors({ credentials: false, origin: true }));
 var http = require("http").createServer(app);
 // var server = app.listen(3000, () => {
 //   console.log("Server is listening on port: 3000");
@@ -16,7 +17,7 @@ var http = require("http").createServer(app);
 // // var http = require("https").createServer(app);
 // const io = require("socket.io").listen(server).origins("*:*");
 
-const io = require("socket.io")(http, { origins: "*:*" });
+const io = require("socket.io")(http);
 
 io.set("origins", "*:*");
 
